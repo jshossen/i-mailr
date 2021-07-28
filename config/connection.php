5419ed2b356b91722940f83af9b38edc8397e3a9
Jakir Hossen <60055998+jshossen@users.noreply.github.com>
2021-07-28 16:51:50 +0600

    Update connection.php

--- config/connection.php
+++ config/connection.php
@@ -1,33 +1,31 @@
 <?php
 //localhost config file
-define("BASE","http://localhost/i-mailr");
+define("BASE","https://apps.assistcafe.com/i-mailr");
 define("START", 2);
 define("TITLE", "Instant Mailr");
 define("DESCRIPTION", "Instant Mailr");
 define("KEYWORDS", "Instant Mailr");
-define("DB_NAME", "bulkmailr");
-// define("DB_USER", "phpmyadmin");
-// define("DB_PASSWORD", "12345");
+define("DB_NAME", "jshossen_i_mailr");
 define("DB_USER", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "Ss128891");
 define("DB_HOST", "localhost");
 
-define("CONTACT_EMAIL", "easymailr@gmail.com");
+define("CONTACT_EMAIL", "jshossen01@gmail.com");
 
 date_default_timezone_set('UTC');
 
-// define("GOOGLE_CLIENT_ID", "720013293800-0hp38eoj1rrlrfm0nm20bnb9hdr58vxx.apps.googleusercontent.com");
-// define("GOOGLE_CLIENT_SECRET", "1Pli0rSell3T2rLEPqkQXpxx");
+// define("GOOGLE_CLIENT_ID", "720013293800-0hp38eoj1rrlrfm0nm20bnb9hdr58v2h.apps.googleusercontent.com");
+// define("GOOGLE_CLIENT_SECRET", "1Pli0rSell3T2rLEPqkQXpyo");
 
-define("GOOGLE_CLIENT_ID", "240980382520-5ekea8stgeelrcqsbfmka7sn5beridsr.apps.googleusercontent.com");
-define("GOOGLE_CLIENT_SECRET", "iArO3ce7Ix35Fnm4VLtaebYU");
+define("GOOGLE_CLIENT_ID", "285506654927-mgbhogvugrka138in3nuhrqpmp4llufe.apps.googleusercontent.com");
+define("GOOGLE_CLIENT_SECRET", "KN8ULlvaIPXaYnm4GQPKA50f");
 
 
-define("PAY_PAL_CLIENTID", "XXXXXX_HqSV3l0CW_NYMOByVXyGcyBKl1i0VRNFPurIMa5Fk6P0bVZB-5Xw9VCpXRuHXCbh9GJYylJhc");
-define("PAY_PAL_SECRET", "XXXXXXXXXXXXGYNtsYFL3GJH9YFuifoxy0Wr6TpYuJk7VYZ5RIUgzmVRTJH8qZu58V_SGZ9Adzq2i4");
+define("PAY_PAL_CLIENTID", "AaA6Fi_HqSV3l0CW_NYMOByVXyGcyBKl1i0VRNFPurIMa5Fk6P0bVZB-5Xw9VCpXRuHXCbh9GJYylJhc");
+define("PAY_PAL_SECRET", "EHBzSO23JHNZyWGYNtsYFL3GJH9YFuifoxy0Wr6TpYuJk7VYZ5RIUgzmVRTJH8qZu58V_SGZ9Adzq2i4");
 
 define("SSLCOMMERZ_API_URL", "https://sandbox.sslcommerz.com/gwprocess/v3/api.php");
-define("SSLCOMMERZ_STORE_ID", "insta5a7e8e4cdexxx");
-define("SSLCOMMERZ_STORE_PASSWORD", "insta5a7e8e4cdexxx@ssl");
+define("SSLCOMMERZ_STORE_ID", "insta5a7e8e4cde53e");
+define("SSLCOMMERZ_STORE_PASSWORD", "insta5a7e8e4cde53e@ssl");
 
 ?>
